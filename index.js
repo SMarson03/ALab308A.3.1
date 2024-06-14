9d38a9145678a598cfcc297cb0e4366a83335fa6
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-06-14 12:24:14 -0400

    2

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-function getUserData(id) {
-  const dbs = {
-    db1: db1,
-    db2: db2,
-    db3: db3
+async function getUserData(id) {
+  const databaseIdentifier= await central(id); 
+  const userData = await dbs[databaseIdentifier](id);
+  const personalData = await vault(id)
+  const userDataObject = {
+    id: id,
+    name: personalData.name,
+    username: userData.username,
+    email: personalData.address,
+    phone: personalData.phone,
+    website: userData.website,
+    company: userData.company
   };
+  return userDataObject
+} catch(error){
+  throw new Error(`Error fetching user data:`)
 }
 
